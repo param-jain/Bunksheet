2e7733e5ccdc99b887bba8491cafc5e15a93c4bd
Param Jain <param_jain@icloud.com>
2018-11-01 13:54:01 +0530

    Stable Initial Boilerplate
    Stable Boilerplate with 2 Tabs:
    1. LoginScreen
    2. LibraryScreen
    using createBottomTabNavigation

--- FrontEnd/bunksheet/App.js
+++ FrontEnd/bunksheet/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+
+import LoginScreen from './screens/LoginScreen';
+import LibraryScreen from './screens/LibraryScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+        login: { screen: LoginScreen },
+        library: { screen: LibraryScreen }
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+          <MainNavigator />
       </View>
     );
   }
@@ -15,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ FrontEnd/bunksheet/screens/LibraryScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LibraryScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+                <Text>LibraryScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default LibraryScreen;
