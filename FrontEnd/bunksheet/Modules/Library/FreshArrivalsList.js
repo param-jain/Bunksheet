94d93de56bce38855cf7e62ff61cb80b3bb04c2e
Param Jain <param_jain@icloud.com>
2019-01-03 23:35:47 +0530

    Fresh Arrivals Book Details Modal Done

--- FrontEnd/bunksheet/Modules/Library/FreshArrivalsList.js
+++ FrontEnd/bunksheet/Modules/Library/FreshArrivalsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Image, Keyboard, ActivityIndicator, StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, ScrollView, Image, Keyboard, ActivityIndicator, StatusBar, KeyboardAvoidingView, TouchableWithoutFeedback, FlatList } from 'react-native';
 import { Header, ListItem } from 'react-native-elements';
+import { LinearGradient } from 'expo';
 
 class FreshArrivalsList extends Component {
 
@@ -10,7 +11,9 @@ class FreshArrivalsList extends Component {
         this.state = {
           loading: false,
           data:[],
-          error: ''
+          error: '',
+          modalVisible:false,
+          bookSelected:[],
         }
 
         this.arrayHolder = [];
@@ -71,28 +74,73 @@ class FreshArrivalsList extends Component {
           />
         );
       };
+
+      bookDetailModal = (item) => {
+        this.setState({bookSelected: item}, () =>{
+          this.setModalVisible(true);
+        });
+      }
   
+      setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+      }  
 
     renderList = () => {
         return (
             <View>
-              <FlatList
-                keyboardShouldPersistTaps='always'
-                data={this.state.data}
-                renderItem={({ item }) => (
-                  <ListItem
-                  roundAvatar
-                  title={item.Title}
-                  subtitle={`A: ${item.Author}  P: ${item.Publisher}`}
-                  leftAvatar={{ source: { uri: item.Image } }}
-                  containerStyle={{ borderBottomWidth: 0 }}
-                  onPress={() => this.toBookDetail()}
-                  />
-                )}
-                keyExtractor={item => item.ISBN}
-                ItemSeparatorComponent={this.renderSeparator}
-                //ListHeaderComponent={this.renderHeader}
-              />
+                <FlatList
+                    keyboardShouldPersistTaps='always'
+                    data={this.state.data}
+                    renderItem={({ item }) => (
+                    <ListItem
+                    roundAvatar
+                    title={item.Title}
+                    subtitle={`A: ${item.Author}  P: ${item.Publisher}`}
+                    leftAvatar={{ source: { uri: item.Image } }}
+                    containerStyle={{ borderBottomWidth: 0 }}
+                    onPress={() => this.bookDetailModal(item)}
+                    />
+                    )}
+                    keyExtractor={item => item.id.toString()}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    //ListHeaderComponent={this.renderHeader}
+                />
+
+                <Modal
+                animationType={'fade'}
+                transparent={true}
+                onRequestClose={() => this.setModalVisible(false)}
+                visible={this.state.modalVisible}>
+
+                    <View style={styles.popupOverlay}>
+                    <View style={styles.popup}>
+                        <View style={styles.popupContent}>
+                        <ScrollView contentContainerStyle={styles.modalInfo}>
+                            <Image style={styles.image} source={{uri: this.state.bookSelected.Image}}/>
+                            <Text style={styles.name}>{this.state.bookSelected.Title}</Text> 
+                            <Text style={styles.author}>Author: {this.state.bookSelected.Author}</Text>
+                            <Text style={styles.publisher}>Publisher: {this.state.bookSelected.Publisher}</Text>
+                            <Text style={styles.about}>"Discover the beautiful science of flowers! Through full-color photos and simple, easy-to-follow text, this nonfiction book introduces emergent readers to the basics of botany, including information on how flowers grow, along with their uses. All Pebble Plus books align with national and state standards and are designed to help new readers read independently, making them the perfect choice for every child."</Text>
+                            <View style={{flexDirection: "row"}}>
+                                <Text style={styles.pageCount}>Page Count: {this.state.bookSelected.pc}</Text>
+                                {this.state.bookSelected.AC > 0 ? <Text style={styles.available}>Available</Text>: <Text style={styles.notAvailable}>Not Available</Text> }
+                            </View>
+                        </ScrollView>
+                        </View>
+                        <View style={styles.popupButtons}>
+                        <LinearGradient
+                        colors={['#FF9800', '#FB8C00', '#EF6C00']}
+                        style={styles.btnClose}
+                        >
+                            <TouchableOpacity onPress={() => {this.setModalVisible(false) }}>
+                            <Text style={styles.txtClose}>Close</Text>
+                            </TouchableOpacity>
+                        </LinearGradient>
+                        </View>
+                    </View>
+                    </View>
+                </Modal>
+
             </View>
         );
     }
@@ -140,7 +188,112 @@ styles={
       height: 35,
       borderRadius: 15,
       margin: 5
-    }
+    },
+
+//Modals Styles
+  popup: {
+    backgroundColor: 'white',
+    marginTop: 80,
+    marginHorizontal: 20,
+    borderRadius: 15,
+  },
+  popupOverlay: {
+    backgroundColor: "#00000057",
+    flex: 1,
+    marginTop: 30
+  },
+  popupContent: {
+    //alignItems: 'center',
+    margin: 5,
+    height:"80%",
+  },
+  popupHeader: {
+    marginBottom: 45
+  },
+  popupButtons: {
+    marginTop: 15,
+    flexDirection: 'row',
+    //borderTopWidth: 1,
+    //borderBottomWidth: 1,
+    borderColor: "#eee",
+    justifyContent:'center'
+  },
+  popupButton: {
+    flex: 1,
+    marginVertical: 16
+  },
+  btnClose:{
+    flex: 0.5,
+    backgroundColor:'#EF6C00',
+    padding:20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
+  },
+  modalInfo:{
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  image:{
+    width:120,
+    height:120,
+    borderRadius:40,
+    marginTop: 10
+  },
+  name:{
+    fontSize:22,
+    flex:1,
+    alignSelf:'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    color:"#FF3D00",
+    fontWeight:'bold',
+    marginTop: 10,
+  },
+  author:{
+    fontSize:16,
+    flex:1,
+    alignSelf:'center',
+    color:"#696969",
+    marginTop: 5
+  },
+  publisher:{
+    fontSize:16,
+    flex:1,
+    alignSelf:'center',
+    color:"#696969",
+  },
+  about:{
+    marginHorizontal:10,
+    marginTop: 10
+  },
+  pageCount: {
+    marginTop:20,
+    marginLeft: 10,
+    flex: 1,
+    color: '#311B92'
+  },
+  notAvailable: {
+    marginTop:20,
+    marginRight: 20,
+    color: 'red',
+    flex: 1,
+    textAlign: 'right'
+  },
+  available: {
+    marginTop:20,
+    marginRight: 20,
+    color: 'green',
+    flex: 1,
+    textAlign: 'right'
+  },
+  txtClose: {
+    alignContent: 'center',
+    justifyContent:'center',
+    color: '#FFFFFF',
+    fontSize: 18,
+    padding: 0
+  }
 }
 
 export default FreshArrivalsList;
